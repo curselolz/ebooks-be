312ee70ebd3ca40d17c280543de01fb597ee6d1b
Oleksandr <vimakek321@mmgaklan.com>
2023-09-20 20:15:17 +0300

    changes

--- controllers/updateBook.ts
+++ controllers/updateBook.ts
@@ -6,15 +6,17 @@ export const updateBook:any = async(context: any) => {
     const id :string = context.params.id;
     let body :any = await context.request.body()
     
-    let data :{email?: String, pno?: String} = {};
-    if(body.value.email){       // if an updated email id is sent
-      data["email"] = body.value.email;
-    }
-    if(body.value.pno){         // if an updated phone no is sent
-      data["pno"] = body.value.pno;
-    }
-    
-    const result = await Books.updateOne({_id: {"$oid": id}}, {$set: data});    
+    let data :{
+      email?: string,
+      country?: String,
+      imageLink?: String,
+      link?: String,
+      language?: String,
+      pages?: String,
+      title?: String,
+      year?: String
+    } = {};
+    const result = await Books.updateOne({_id: {"$id": id}}, {$set: data});    
     
     context.response.body = result;
     context.response.status = 200;
